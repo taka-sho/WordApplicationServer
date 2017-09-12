3867f6deeb30bbaa14e92dd4b42dcb1ccbf0ad03
JpnLavender <irimamekun@gmail.com>
2017-09-12 09:40:39 +0900

    Setting Oath Twitter

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
 end
