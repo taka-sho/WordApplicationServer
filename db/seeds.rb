a1e73b71abc11995956fb9e49447a6dd1ec88900
JpnLavender <irimamekun@gmail.com>
2017-09-22 23:09:38 +0900

    Add Word Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+Word.new(japanese: "トレーニング", english: "training").save
+Word.new(japanese: "チャレンジ", english: "challenge").save
+Word.new(japanese: "コマンド", english: "command").save
+Word.new(japanese: "翻訳", english: "translate").save
+Word.new(japanese: "データ", english: "data").save
+Word.new(japanese: "日にち", english: "date").save
+Word.new(japanese: "情報", english: "infomation").save
+Word.new(japanese: "お金", english: "money").save
+Word.new(japanese: "りんご", english: "Apple").save
+Word.new(japanese: "バナナ", english: "banana").save
+Word.new(japanese: "もも", english: "peach").save
+Word.new(japanese: "ビジネス", english: "business").save
+Word.new(japanese: "会社", english: "company").save
+Word.new(japanese: "事務所", english: "office").save
+Word.new(japanese: "欲しい", english: "want").save
+Word.new(japanese: "店", english: "store").save
+Word.new(japanese: "方法", english: "way").save
+Word.new(japanese: "注文", english: "order").save
+Word.new(japanese: "call", english: "呼ぶ").save
+Word.new(japanese: "サービス", english: "service").save
+Word.new(japanese: "サーバー", english: "server").save
+Word.new(japanese: "基本", english: "base").save
+Word.new(japanese: "報告", english: "report").save
+Word.new(japanese: "市場", english: "market").save
+Word.new(japanese: "変わる", english: "change").save
+Word.new(japanese: "受け取る", english: "eceive").save
+Word.new(japanese: "含む", english: "include").save
